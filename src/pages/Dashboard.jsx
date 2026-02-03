aeda08559a59aeacea43292c1ea8bf410244a219
m-ades <m-ades@users.noreply.github.com>
2026-02-03 16:16:23 -0500

    change whats left percentage in the donut chart to remaining assignments

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -423,7 +423,7 @@ export default function Dashboard() {
                     fontWeight="medium"
                     sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
                   >
-                    {releaseOverview.pastDuePercent}%
+                    {releaseOverview.remainingPercent}%
                   </Typography>
                   <ResponsiveContainer width="100%" height={200}>
                     <PieChart>
